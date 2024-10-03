357eb63ebbedb3e6aa07d05dcd068d790feeb3e4
EnchiIchito <ichitonton@gmail.com>
2024-10-03 15:34:45 +0900

    no message

---
+++ インターン作品/Assets/Script/ChangeScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void change_button(string sceneName)
+    {
+        if (sceneName == "esc")
+        {
+            UnityEngine.Application.Quit();
+        }
+        else
+        {
+            Debug.Log("Press the button");
+            SceneManager.LoadScene(sceneName);//sceneÇåƒÇ—èoÇµÇ‹Ç∑
+        }
+    }
+}
