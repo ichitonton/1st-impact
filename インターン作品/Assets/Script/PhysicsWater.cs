d6ce71dcaa4d881d3279983fc01c62c5f991c5a9
ichitonton <ichtonton@gmail.com>
2024-10-08 15:46:44 +0900

    10/8 修正

--- インターン作品/Assets/Script/PhysicsWater.cs
+++ インターン作品/Assets/Script/PhysicsWater.cs
@@ -7,7 +7,7 @@ public class PhysicsWater : MonoBehaviour
     public GameObject water;
     Rigidbody2D rigid;
     public float buoyancy; //•‚—Í
-    public float defaultPositionX = -5.0f;
+    public float moveSpeed = 1.0f;
     float distansFromDefault;
     // Start is called before the first frame update
     void Start()
@@ -20,8 +20,8 @@ public class PhysicsWater : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distansFromDefault = defaultPositionX - gameObject.GetComponent<Transform>().position.x;
-        rigid.velocityX = distansFromDefault * 0.1f;
+        //distansFromDefault = defaultPositionX - gameObject.GetComponent<Transform>().position.x;
+        //rigid.velocityX = distansFromDefault * 0.1f;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -29,12 +29,14 @@ public class PhysicsWater : MonoBehaviour
         //…‚ÆÚG‚µ‚Ä‚¢‚½‚ç
         if (collision.gameObject == water)
         {
-            rigid.gravityScale = 0.5f;
             //Debug.Log("tatch water");
+
+            rigid.gravityScale = 0.5f;
+            
             //…’ïR
             rigid.velocity *= 0.98f;
 
-            //ãŒü‚«‚É‰Á‘¬“x‚ğ‰Á‚¦‚é
+            //ãŒü‚«‚É‰Á‘¬“x‚ğ‰Á‚¦‚é(•‚—Í)
             Vector2 force = new Vector2(0.0f, buoyancy);
 
             rigid.AddForce(force, ForceMode2D.Force);
